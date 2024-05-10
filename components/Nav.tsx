ebce14c9bfbf1beac969427ffd8f8045f3b6dc30
Daniel Björlund <bjorlund@gmx.com>
2024-05-10 12:20:37 +0200

    Burger menu done

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,22 @@
 "use client"
 import Link from "next/link"
 import Image from "next/image" // Automatic optimize images for us ???
+import React, { useState } from 'react';
+
 
  // Måste ha client side rendering när vi använder hooks
 
 const Nav = () => {
 
-
-
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  //Handles the opening and closing of our nav
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+};
     
   return (
-    <nav className="flex justify-between w-full mb-4 p-4  bg_col_prim fixed">
+    <nav className="flex justify-between w-full mb-4 p-4 bg_col_prim fixed">
       <Link href="/" className="flex">
         <Image 
           src="/pmlogoa.png" 
@@ -21,13 +27,21 @@ const Nav = () => {
         />
       </Link>
 
-      <p className="text_col_sec">PlusMinus App</p>
+      <p className="text_col_sec text-xl font-bold">PlusMinus App</p>
       
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        MOB NAVIGATION
+        <button onClick={handleClick} className="flex flex-col justify-center items-center">
+          <span className={`bg_col_sec block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`} >
+          </span>
+          <span className={`bg_col_sec block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-1 ${menuOpen ? 'opacity-0' : 'opacity-100'}`} >
+          </span>
+          <span className={`bg_col_sec block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? '-rotate-45 -translate-y-2' : 'translate-y-0.5'}`} >
+          </span> 
+        </button>
       </div>
     </nav>
+    
   )
 }
 
